8fd5b7f890fe1dbad29800785aa9055a55a0d02b
Nikolai Ulianov <arka.94@mail.ru>
2020-03-11 18:19:56 +0300

    Updates

---
+++ precache-manifest.f4e8fab49c1c34e533afefffaaa4e258.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee87a3029d493930a800df6682aeb44",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5a400a259b72e54b5d5a",
+    "url": "/react/static/css/4.28141dab.chunk.css"
+  },
+  {
+    "revision": "fd61d0aea47d4fc15d84",
+    "url": "/react/static/css/5.adc8c516.chunk.css"
+  },
+  {
+    "revision": "2f3f15bb4a03169fb31f",
+    "url": "/react/static/css/6.f503d35c.chunk.css"
+  },
+  {
+    "revision": "6c3920855f4a539f2422",
+    "url": "/react/static/css/7.393989ec.chunk.css"
+  },
+  {
+    "revision": "1cf3b96a2cfc9cdee596",
+    "url": "/react/static/css/main.90ec2377.chunk.css"
+  },
+  {
+    "revision": "bb3a97da165e7f090d4c",
+    "url": "/react/static/js/0.10e54f19.chunk.js"
+  },
+  {
+    "revision": "b4465ff699e66644ed6c",
+    "url": "/react/static/js/3.f815c0cc.chunk.js"
+  },
+  {
+    "revision": "5a400a259b72e54b5d5a",
+    "url": "/react/static/js/4.8c7a8b01.chunk.js"
+  },
+  {
+    "revision": "fd61d0aea47d4fc15d84",
+    "url": "/react/static/js/5.d3a2e78e.chunk.js"
+  },
+  {
+    "revision": "2f3f15bb4a03169fb31f",
+    "url": "/react/static/js/6.40176017.chunk.js"
+  },
+  {
+    "revision": "6c3920855f4a539f2422",
+    "url": "/react/static/js/7.acb59ea5.chunk.js"
+  },
+  {
+    "revision": "1cf3b96a2cfc9cdee596",
+    "url": "/react/static/js/main.3981bacd.chunk.js"
+  },
+  {
+    "revision": "8ee1bb5b308fbfc03dec",
+    "url": "/react/static/js/runtime-main.d7fa3f23.js"
+  },
+  {
+    "revision": "a225a70049ae219b58de0155ef62aa79",
+    "url": "/react/static/media/Preloader.a225a700.svg"
+  }
+]);
